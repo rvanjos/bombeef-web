3e8bc8b20f77e67664f2c7313be0132338477d94
rvanjos <rvanjos@gmail.com>
2026-09-22 09:37:20 -0300

    feat: carregar planejamento e conciliação no DRE

--- public/js/dre-conferencia.js
+++ public/js/dre-conferencia.js
@@ -11,7 +11,8 @@
       '<script src="/js/dre-cartao-pagamentos.js?v=1" data-dre-cartao-pag="1"><\/script>' +
       '<script src="/js/dre-conferencia-v2.js?v=2" data-dre-conf-v2="1"><\/script>' +
       '<script src="/js/dre-lancamentos-area.js?v=3" data-dre-lanc-area="1"><\/script>' +
-      '<script src="/js/dre-fechamento.js?v=2" data-dre-fechamento="1"><\/script>';
+      '<script src="/js/dre-fechamento.js?v=2" data-dre-fechamento="1"><\/script>' +
+      '<script src="/js/dre-planejamento-conciliacao.js?v=1" data-dre-plan-conc="1"><\/script>';
   }
   if (typeof document !== 'undefined') {
     if (document.readyState === 'loading') {
@@ -25,7 +26,9 @@
           var p=document.createElement('script'); p.src='/js/dre-cartao-pagamentos.js?v=1'; p.dataset.dreCartaoPag='1'; p.onload=function(){
             var q=document.createElement('script'); q.src='/js/dre-conferencia-v2.js?v=2'; q.dataset.dreConfV2='1'; q.onload=function(){
               var a=document.createElement('script'); a.src='/js/dre-lancamentos-area.js?v=3'; a.dataset.dreLancArea='1'; a.onload=function(){
-                var f=document.createElement('script'); f.src='/js/dre-fechamento.js?v=2'; f.dataset.dreFechamento='1'; document.head.appendChild(f);
+                var f=document.createElement('script'); f.src='/js/dre-fechamento.js?v=2'; f.dataset.dreFechamento='1'; f.onload=function(){
+                  var z=document.createElement('script'); z.src='/js/dre-planejamento-conciliacao.js?v=1'; z.dataset.drePlanConc='1'; document.head.appendChild(z);
+                }; document.head.appendChild(f);
               }; document.head.appendChild(a);
             }; document.head.appendChild(q);
           }; document.head.appendChild(p);
