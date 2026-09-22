6bcad68316749eb211084a5109a8e26f51a2b258
rvanjos <rvanjos@gmail.com>
2026-09-22 09:44:44 -0300

    chore: atualizar cache da conciliação e planejamento

--- public/js/dre-conferencia.js
+++ public/js/dre-conferencia.js
@@ -12,7 +12,7 @@
       '<script src="/js/dre-conferencia-v2.js?v=2" data-dre-conf-v2="1"><\/script>' +
       '<script src="/js/dre-lancamentos-area.js?v=4" data-dre-lanc-area="1"><\/script>' +
       '<script src="/js/dre-fechamento.js?v=2" data-dre-fechamento="1"><\/script>' +
-      '<script src="/js/dre-planejamento-conciliacao.js?v=2" data-dre-plan-conc="1"><\/script>';
+      '<script src="/js/dre-planejamento-conciliacao.js?v=3" data-dre-plan-conc="1"><\/script>';
   }
   if (typeof document !== 'undefined') {
     if (document.readyState === 'loading') {
@@ -27,7 +27,7 @@
             var q=document.createElement('script'); q.src='/js/dre-conferencia-v2.js?v=2'; q.dataset.dreConfV2='1'; q.onload=function(){
               var a=document.createElement('script'); a.src='/js/dre-lancamentos-area.js?v=4'; a.dataset.dreLancArea='1'; a.onload=function(){
                 var f=document.createElement('script'); f.src='/js/dre-fechamento.js?v=2'; f.dataset.dreFechamento='1'; f.onload=function(){
-                  var z=document.createElement('script'); z.src='/js/dre-planejamento-conciliacao.js?v=2'; z.dataset.drePlanConc='1'; document.head.appendChild(z);
+                  var z=document.createElement('script'); z.src='/js/dre-planejamento-conciliacao.js?v=3'; z.dataset.drePlanConc='1'; document.head.appendChild(z);
                 }; document.head.appendChild(f);
               }; document.head.appendChild(a);
             }; document.head.appendChild(q);
